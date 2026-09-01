b58b34537f3263966b8ee077032696597913b56d
brasotaste-lgtm <brasotaste@gmail.com>
2026-09-01 18:16:19 -0300

    Delete chef.tsx

--- chef.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/chef")({
-  beforeLoad: () => {
-    throw redirect({ to: "/sobre", hash: "chef" });
-  },
-});
